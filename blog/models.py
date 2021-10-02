d73be234da8a9b9289638dc758dbe8c68178b464
amirine <zhuk.irine@gmail.com>
2021-10-02 23:54:08 +0300

    New models Post and Category created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Title')
+    date = models.DateTimeField(auto_now=True, verbose_name='Date')
+    text = models.TextField(max_length=200, verbose_name='Text Field')
+    category = models.ForeignKey('Category', null=False, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ['-date']
+
+    def __str__(self):
+        return f'{self.category}: {self.title}'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Name')
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name

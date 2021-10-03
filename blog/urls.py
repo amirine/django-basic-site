4eb42e77563a25f09f4e146c3f0a4ffb1cd810af
amirine <zhuk.irine@gmail.com>
2021-10-03 15:58:11 +0300

    Added GenericAPI for Post model

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import PostView
+from .views import PostView, SinglePostView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('posts/', PostView.as_view(), name='api_posts'),
-    path('posts/<int:pk>/', PostView.as_view(), name='api_by_post')
+    path('posts/<int:pk>/', SinglePostView.as_view(), name='api_by_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,39 +1,19 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-from .models import Post
+from .models import Post, Category
 from .serializers import PostSerializer
 
 
-class PostView(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response({"posts": serializer.data})
+class PostView(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    def post(self, request):
-        new_post = request.data.get('post')
-        serializer = PostSerializer(data=new_post)
-        if serializer.is_valid(raise_exception=True):
-            new_post_saved = serializer.save()
-        return Response({
-            "success": f"Post {new_post_saved.title} created successfully."
-        })
+    def perform_create(self, serializer):
+        category = get_object_or_404(Category, id=self.request.data.get('category'))
+        return serializer.save(category=category)
 
-    def put(self, request, pk):
-        saved_post = get_object_or_404(Post.objects.all(), pk=pk)
-        data = request.data.get('post')
-        serializer = PostSerializer(instance=saved_post, data=data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            post_saved = serializer.save()
-        return Response({
-            "success": f"Post {post_saved.title} updated successfully."
-        })
 
-    def delete(self, request, pk):
-        post_to_delete = get_object_or_404(Post, pk=pk)
-        post_to_delete.delete()
-        return Response({
-            "success": f"Post with id {pk} successfully deleted."
-        }, status=204)
+class SinglePostView(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
